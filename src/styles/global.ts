f920ccb008c242659381dd8b88e5dc923b8c80c6
julia <juliabresolin2@gmail.com>
2022-09-17 17:00:39 -0300

    style: create global style

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: ${(props) => props.theme.colors['green-500']}; 
+  }
+
+  body {
+    background-color: ${(props) => props.theme.colors['gray-800']};
+    color: ${(props) => props.theme.colors['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem ${(props) => props.theme.fonts.default}
+  }
+`
