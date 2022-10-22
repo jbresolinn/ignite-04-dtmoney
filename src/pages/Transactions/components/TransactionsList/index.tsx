2801d3d39ad8c8b84016091926efc3961f582518
julia <juliabresolin2@gmail.com>
2022-10-21 21:33:47 -0300

    ✨ create transactions context

--- src/pages/Transactions/components/TransactionsList/index.tsx
+++ src/pages/Transactions/components/TransactionsList/index.tsx
@@ -1,44 +1,27 @@
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 import { PriceHighlight, TransactionsListContainer } from './styles'
 
 export function TransactionsList() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <TransactionsListContainer>
       <tbody>
-        <tr>
-          <td>Desenvolvimento de site</td>
-          <td>
-            <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-          </td>
-          <td>Venda</td>
-          <td>13/04/2022</td>
-        </tr>
-
-        <tr>
-          <td>Desenvolvimento de site</td>
-          <td>
-            <PriceHighlight variant="outcome">R$ -59,00</PriceHighlight>
-          </td>
-          <td>Alimentação</td>
-          <td>13/04/2022</td>
-        </tr>
-
-        <tr>
-          <td>Desenvolvimento de site</td>
-          <td>
-            <PriceHighlight variant="outcome">R$ -59,00</PriceHighlight>
-          </td>
-          <td>Alimentação</td>
-          <td>13/04/2022</td>
-        </tr>
-
-        <tr>
-          <td>Desenvolvimento de site</td>
-          <td>
-            <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-          </td>
-          <td>Venda</td>
-          <td>13/04/2022</td>
-        </tr>
+        {transactions.map((transaction) => {
+          return (
+            <tr key={transaction.id}>
+              <td>{transaction.description}</td>
+              <td>
+                <PriceHighlight variant={transaction.type}>
+                  {transaction.price}
+                </PriceHighlight>
+              </td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          )
+        })}
       </tbody>
     </TransactionsListContainer>
   )
