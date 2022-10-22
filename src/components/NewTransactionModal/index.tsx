2d299e355adfabe3498a28ef3b32658a39e9ee5d
julia <juliabresolin2@gmail.com>
2022-10-21 22:55:22 -0300

    ✨ create new transaction feature

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as AlertDialog from '@radix-ui/react-alert-dialog'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import { useContext } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import * as z from 'zod'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 import {
   CloseButton,
   Content,
@@ -21,9 +23,12 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
+  const { createNewTransaction } = useContext(TransactionsContext)
+
   const {
     control,
     register,
+    reset,
     handleSubmit,
     formState: { isSubmitting },
   } = useForm<NewTransactionFormInputs>({
@@ -36,7 +41,8 @@ export function NewTransactionModal() {
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
     await new Promise((resolve) => setTimeout(resolve, 2000))
 
-    console.log(data)
+    createNewTransaction(data)
+    reset()
   }
 
   return (
--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
+import { api } from '../lib/axios'
 
 export interface Transaction {
   id: number
@@ -9,9 +10,17 @@ export interface Transaction {
   createdAt: string
 }
 
+export interface NewTransaction {
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+}
+
 interface TransactionsContextType {
   transactions: Transaction[]
   fetchTransactions: (query?: string) => Promise<void>
+  createNewTransaction: (newTransaction: NewTransaction) => Promise<void>
 }
 
 interface TransactionsProviderProps {
@@ -24,16 +33,24 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
   async function fetchTransactions(query?: string) {
-    const url = new URL('http://localhost:3000/transactions')
+    const response = await api.get('transactions', {
+      params: {
+        q: query,
+        _sort: 'createdAt',
+        _order: 'desc',
+      },
+    })
 
-    if (query) {
-      url.searchParams.append('q', query)
-    }
+    setTransactions(response.data)
+  }
 
-    const response = await fetch(url)
-    const data = await response.json()
+  async function createNewTransaction(data: NewTransaction) {
+    const response = await api.post('transactions', {
+      ...data,
+      createdAt: new Date(),
+    })
 
-    setTransactions(data)
+    setTransactions((state) => [response.data, ...state])
   }
 
   useEffect(() => {
@@ -41,7 +58,9 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createNewTransaction }}
+    >
       {children}
     </TransactionsContext.Provider>
   )
